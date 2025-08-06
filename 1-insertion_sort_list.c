d28c491a6133510600502bfcb4ed72efc04779ea
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-06 09:00:45 +0000

    Save progress 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * insertion_sort_list - sorts list in ascending order using insertion sort
+ * @list: double pointer to head of doubly linked list
+ *
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
