dd7fa7fbb49ce1c37a48f29360ed2d74028f1608
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-07 02:15:37 +0000

    add 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,27 +11,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int min_index, temp;
+	size_t i, j, min_index;
+	int temp;
 
-	while (i < size)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 
-		while (j < size)
+		for (j = i + 1; j < size; j++)
 		{
-			j = i + 1;
-			j = j + 1;
-		if (array[j] < array[min_index])
+			if (array[j] < array[min_index])
 			{
 				min_index = j;
-				print_array(array, size);
 			}
 		}
-		i = i + 1;
-	}
-
-		temp = array[i];
+		temp = arr[i];
 		array[i] = array[min_index];
 		array[min_index] = temp;
+		print_array(array, size);
+	}
 }
