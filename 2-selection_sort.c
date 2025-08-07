bb87697391b4bfcc8b3b50c95e80828787354d1a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-07 01:51:06 +0000

    add 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,7 +25,7 @@ void selection_sort(int *array, size_t size)
 		if (array[j] < array[min_index])
 			{
 				min_index = j;
-				print_array(*array, size);
+				print_array(array, size);
 			}
 		}
 		i = i + 1;
