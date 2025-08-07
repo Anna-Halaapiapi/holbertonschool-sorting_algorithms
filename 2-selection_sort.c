76993d073da4a17107a4836b24b788e6b6484bba
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-07 01:47:36 +0000

    add 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * selection_sort - sorts array of ints in ascending order using selection sort
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int min_index, temp;
+
+	while (i < size)
+	{
+		min_index = i;
+
+		while (j < size)
+		{
+			j = i + 1;
+			j = j + 1;
+		if ([array[j] < array[min_index])
+			{
+				min_index = j;
+				print_list(*list);
+			}
+		}
+		i = i + 1;
+	}
+
+		temp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = temp;
+}
